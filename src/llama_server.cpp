c0395cc37ad2e64da943cf55bb2e661f7a66b551
zoddnodd <48840371+zoddnodd@users.noreply.github.com>
2024-10-25 23:38:32 -0300

    feat: Llama Apihook implementation

--- src/llama_server.cpp
+++ src/llama_server.cpp
@@ -7,12 +7,186 @@
 #include "llama_server.hpp"
 
 
+#include "config/configmanager.hpp"
+#include "game/scheduling/dispatcher.hpp"
+#include "utils/tools.hpp"
+#include "lib/di/container.hpp"
+
+
+
 // Callback function to handle the data received from the API
-size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
-{
-    ((std::string*)userp)->append((char*)contents, size * nmemb);
-    return size * nmemb;
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	((std::string*)userp)->append((char*)contents, size * nmemb);
+	return size * nmemb;
 }
+
+
+Apihook::Apihook(ThreadPool &threadPool) :
+	threadPool(threadPool) {
+	if (curl_global_init(CURL_GLOBAL_ALL) != 0) {
+		g_logger().error("Failed to init curl, no APIhook messages may be sent");
+		return;
+	}
+
+	headers = curl_slist_append(headers, "content-type: application/json");
+
+	if (headers == NULL) {
+		g_logger().error("Failed to init curl, appending request headers failed");
+		return;
+	}
+
+	run();
+}
+
+Apihook &Apihook::getInstance() {
+	return inject<Apihook>();
+}
+
+void Apihook::run() {
+	threadPool.detach_task([this] { sendApihook(); });
+	g_dispatcher().scheduleEvent(
+		g_configManager().getNumber(DISCORD_WEBHOOK_DELAY_MS), [this] { run(); }, "Apihook::run"
+	);
+}
+
+void Apihook::sendPayload(const std::string &payload, std::string url) {
+	std::scoped_lock lock { taskLock };
+	apihooks.push_back(std::make_shared<ApiTask>(payload, url));
+}
+
+void Apihook::sendMessage(const std::string &message, std::string url) {
+	if (url.empty()) {
+		url = g_configManager().getString(DISCORD_WEBHOOK_URL);
+	}
+
+	if (url.empty() || message.empty()) {
+		return;
+	}
+
+	sendPayload(getPayload("", message, -1, false), url);
+}
+
+
+int Apihook::sendRequest(const char* url, const char* payload, std::string* response_body) const {
+	CURL* curl = curl_easy_init();
+	if (!curl) {
+		g_logger().error("Failed to send webhook message; curl_easy_init failed");
+		return -1;
+	}
+
+	curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:11434/api/generate");
+
+	auto stand_request = std::string("Please, answer the previous question but consider that you're a NPC from the city of Carlin from the world of Tibia and a city from the medivil. You must only know about the world of Tibia and if anyone asks about today's things you answer that you have no idea what it is.Can you answer in a really small sentence?");
+
+	// JSON data to send
+	std::string jsonData = R"({
+            "model": "llama3.2",
+            "prompt": "Hi! What do you know about Brazil?)"
+		+ stand_request + R"(", 
+            "stream": false,
+            "options": {
+                 "temperature": 0.6
+            }
+        })";
+
+		curl_easy_setopt(curl, CURLOPT_POSTFIELDS, jsonData.c_str());
+
+
+
+	curl_easy_setopt(curl, CURLOPT_URL, url);
+
+	curl_easy_setopt(curl, CURLOPT_POSTFIELDS, payload);
+	curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, &Apihook::writeCallback);
+
+
+	CURLcode res = curl_easy_perform(curl);
+
+	if (res != CURLE_OK) {
+		g_logger().error("Failed to send webhook message with the error: {}", curl_easy_strerror(res));
+		curl_easy_cleanup(curl);
+
+		return -1;
+	}
+
+	int response_code;
+
+	curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+	curl_easy_cleanup(curl);
+
+	return response_code;
+}
+
+std::string Apihook::getPayload(const std::string &title, const std::string &message, int color, bool embed) const {
+	std::time_t now = getTimeNow();
+	std::string time_buf = formatDate(now);
+
+	std::stringstream footer_text;
+	footer_text
+		<< g_configManager().getString(SERVER_NAME) << " | "
+		<< time_buf;
+
+	std::stringstream payload;
+	if (embed) {
+		payload << "{ \"embeds\": [{ ";
+		payload << "\"title\": \"" << title << "\", ";
+		if (!message.empty()) {
+			payload << "\"description\": \"" << message << "\", ";
+		}
+		if (g_configManager().getBoolean(DISCORD_SEND_FOOTER)) {
+			payload << "\"footer\": { \"text\": \"" << footer_text.str() << "\" }, ";
+		}
+		if (color >= 0) {
+			payload << "\"color\": " << color;
+		}
+		payload << " }] }";
+	} else {
+		payload << "{ \"content\": \"" << (!message.empty() ? message : title) << "\" }";
+	}
+
+	return payload.str();
+}
+
+void Apihook::sendApihook() {
+	std::scoped_lock lock { taskLock };
+	if (apihooks.empty()) {
+		return;
+	}
+
+	const auto &task = apihooks.front();
+
+	std::string response_body;
+	auto response_code = sendRequest(task->url.c_str(), task->payload.c_str(), &response_body);
+
+	if (response_code == -1) {
+		return;
+	}
+
+	if (response_code == 429 || response_code == 504) {
+		g_logger().warn("Webhook encountered error code {}, re-queueing task.", response_code);
+
+		return;
+	}
+
+	if (response_code >= 300) {
+		g_logger().error(
+			"Failed to send webhook message, error code: {} response body: {} request body: {}",
+			response_code,
+			response_body,
+			task->payload
+		);
+
+		return;
+	}
+}
+
+size_t Apihook::writeCallback(void* contents, size_t size, size_t nmemb, void* userp) {
+	size_t real_size = size * nmemb;
+	auto* str = reinterpret_cast<std::string*>(userp);
+	str->append(reinterpret_cast<char*>(contents), real_size);
+	return real_size;
+}
+
+
 
 bool static llama_connect() {
 	CURL* curl;
--- src/llama_server.hpp
+++ src/llama_server.hpp
@@ -12,6 +12,50 @@
 #include "security/rsa.hpp"
 #include "server/server.hpp"
 
+#include "lib/thread/thread_pool.hpp"
+
+
+
+struct ApiTask {
+	std::string payload;
+	std::string url;
+};
+
+
+
+class Apihook {
+public:
+	static constexpr size_t DEFAULT_DELAY_MS = 1000;
+
+	explicit Apihook(ThreadPool &threadPool);
+	// Singleton - ensures we don't accidentally copy it
+	Apihook(const Apihook &) = delete;
+	void operator=(const Apihook &) = delete;
+
+	static Apihook &getInstance();
+
+	void run();
+
+	void sendPayload(const std::string &payload, std::string url);
+	void sendMessage(const std::string &title, const std::string &message, int color, std::string url = "", bool embed = true);
+	void sendMessage(const std::string &message, std::string url = "");
+
+
+private:
+	std::mutex taskLock;
+	ThreadPool & threadPool;
+	std::deque<std::shared_ptr<ApiTask>> apihooks;
+	curl_slist* headers = nullptr;
+
+	void sendApihook();
+
+	int sendRequest(const char* url, const char* payload, std::string* response_body) const;
+	static size_t writeCallback(void* contents, size_t size, size_t nmemb, void* userp);
+	std::string getPayload(const std::string &title, const std::string &message, int color, bool embed = true) const;
+};
+
+constexpr auto g_apihook = Apihook::getInstance;
+
 bool static llama_connect();
 
 std::string llamaSendText();
